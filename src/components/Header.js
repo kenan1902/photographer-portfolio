ab457e53115e14e1a1add2b1ac4592d693535c0b
Kenan <kenancaparov@gmail.com>
2024-08-27 12:36:01 +0400

    v1

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-import Reac, { useContext } from "react";
+import React, { useContext } from "react";
 import Socials from "./Socials";
 import Logo from "../img/header/logo.svg";
 import MobileNav from "./MobileNav";
