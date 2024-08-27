a06fbf1136b146a71078cbeea17f4c4cb40c1b28
Kenan <kenancaparov@gmail.com>
2024-08-27 12:11:52 +0400

    v1

---
+++ src/components/Header.js
@@ -0,0 +1,58 @@
+import Reac, { useContext } from "react";
+import Socials from "./Socials";
+import Logo from "../img/header/logo.svg";
+import MobileNav from "./MobileNav";
+import { Link } from "react-router-dom";
+import { CursorContext } from "../context/CursorContext";
+
+const Header = () => {
+  const { mouseEnterHandler, mouseLeaveHandler } = useContext(CursorContext);
+  return (
+    <header className="fixed w-full px-[30px] lg:px-[100px] z-30 h-[100px] lg:h-[140px] flex items-center">
+      <div className="flex flex-col lg:flex-row lg:items-center w-full justify-between">
+        <Link
+          to={"/"}
+          className="max-w-[200px]"
+          onMouseEnter={mouseEnterHandler}
+          onMouseLeave={mouseLeaveHandler}
+        >
+          <img src={Logo} alt=""></img>
+        </Link>
+        <nav
+          className="hidden xl:flex gap-x-12 font-semibold"
+          onMouseEnter={mouseEnterHandler}
+          onMouseLeave={mouseLeaveHandler}
+        >
+          <Link
+            to={"/"}
+            className="text-[#696c6d] hover:text-primary transition"
+          >
+            Home
+          </Link>
+          <Link
+            to={"/about"}
+            className="text-[#696c6d] hover:text-primary transition"
+          >
+            About
+          </Link>
+          <Link
+            to={"/portfolio"}
+            className="text-[#696c6d] hover:text-primary transition"
+          >
+            Portfolio
+          </Link>
+          <Link
+            to={"/contact"}
+            className="text-[#696c6d] hover:text-primary transition"
+          >
+            Contact
+          </Link>
+        </nav>
+      </div>
+      <Socials />
+      <MobileNav />
+    </header>
+  );
+};
+
+export default Header;
